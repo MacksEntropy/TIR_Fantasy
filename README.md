82b02b5697b6bf2e8044f8a42ec3b653cb46e2bb
MacksEntropy <andrescovarrubias27@gmail.com>
2025-02-27 10:25:56 -0600

    Update model in code

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ where
 
 $$
 \begin{align*}
-    \text{Speed Score}\qquad S = \frac{\text{Actual Time}}{\text{Goal Time}} \\
+    \text{Speed Score}\qquad S = \frac{\text{Goal Time}}{\text{Actual Time}} \\
 \text{Pace Adherence} \qquad A = \left(\frac{{\text{Actual Pace - Goal Pace}}}{\text{Goal Pace}}\right)^2 + \frac{{\text{Actual Pace - Goal Pace}}}{\text{Goal Pace}} \\
 \text{Difficulty Score}\qquad D = 100 - \left(\left(\text{Rank} - 1\right)\times\frac{8}{7}\right)
 \end{align*}
@@ -23,4 +23,4 @@ This gives the general form of the performance score (up to a scaling constant):
 
 ![Performance Score Form](performance_score_form.PNG)
 
-The weights $w_{s}, w_{a}$ are predetermined to mainly favor reaching a goal pace. $\text{Rank}$ is metric ranging from 1 (hardest) to 36 (easiest) which indexes the difficulty of each race leg. Rankings are based on everything else being equal. So, neither the time of day (or which day) the legs are run, nor the weather is considered in the ranking. The largest factor is the length of the leg, followed by changes in elevation, and number of turns to navigate. 
+The weights $w_{s}, w_{a}$ are predetermined to mainly favor reaching a goal pace. $\text{Rank}$ is metric ranging from 1 (hardest) to 36 (easiest) which indexes the difficulty of each race leg. Rankings are based on everything else being equal. So, neither the time of day (or which day) the legs are run, nor the weather is considered in the ranking. The largest factor is the length of the leg, followed by changes in elevation, and number of turns to navigate.
--- main.py
+++ main.py
@@ -3,32 +3,35 @@ import numpy as np
 
 class TIRFantasy:
 
-    def load_data(self, race_data: str, runner_data: str):
+    def load_data(self, race_data: str, runner_data: str) -> None:
         """Load data from external CSVs
 
         Args:
             race_data (str) : Path to CSV containing race data
             runner_data (str) : Path to CSV containing runner data
-        
         """
         self.legs = pd.read_csv(race_data)
         self.runners = pd.read_csv(runner_data)
 
-    def calculate_speed_score(self, goal_time, actual_time):
-        return (goal_time/actual_time) * 100
+    def speed_score(self, goal_time : int, actual_time : int) -> float:
+        return (goal_time/actual_time)
 
-    def calculate_pace_adherence(self, goal_pace, actual_pace):
-        return (1-((abs(actual_pace-goal_pace))/goal_pace)) * 100
+    def pace_adherence(self, goal_pace : int, actual_pace : int) -> float:
+        return ((actual_pace-goal_pace)/goal_pace)^2 + ((actual_pace-goal_pace)/goal_pace)
     
-    def calculate_difficulty_score(self, rank):
+    def difficulty_score(self, rank : int) -> float:
         """ Calculate the difficulty score of a given leg based on the rank (column race data csv). Maps the rankings [1,36] to a 
         difficulty score [100,0].
         """
-        return (1 - ((rank - 1)/(36-1))) * 100
+        return 100 - ((rank-1) * (8/7))
 
-    def calculate_overall_score(self, speed_score, pace_adherence, difficulty_score):
-        return difficulty_score * ((0.25 * speed_score) + (0.75 * pace_adherence)) / 100
-
+    def performance_score(self, speed_score : float, pace_adherence : float, difficulty_score: float) -> float:
+        """
+        Calculate overall score where speed score has a 25% weight, pace adherence has a 75% weight, and the composite score is weighted 
+        by the difficulty score of a given leg.
+        """
+        return difficulty_score * ((0.25 * speed_score) - (0.75 * pace_adherence))
+    
 if __name__ =="__main__":
     tir = TIRFantasy()
     tir.load_data("dummy_race_data.csv", "runner_data.csv")
